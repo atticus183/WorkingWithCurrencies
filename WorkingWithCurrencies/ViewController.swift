e55ac6fc2949ce9ec1b798d0b614f894abab344d
Josh R <seventytwoapp@gmail.com>
2019-12-16 23:51:52 -0600

    Initial Commit

---
+++ WorkingWithCurrencies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorkingWithCurrencies
+//
+//  Created by Josh R on 12/16/19.
+//  Copyright © 2019 Josh R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
