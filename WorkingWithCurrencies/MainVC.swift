037008f344b6522fb9abdd266be171f94fdade50
Josh R <seventytwoapp@gmail.com>
2019-12-16 23:58:25 -0600

    Deleted all references to the storyboard.  Starting point.

--- WorkingWithCurrencies/MainVC.swift
+++ WorkingWithCurrencies/MainVC.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .systemBlue
     }
 
 
